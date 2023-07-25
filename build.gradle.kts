33a8587ad2453c21f488dbcaccf79f8e5d386d28
Joffrey Bion <joffrey.bion@gmail.com>
2023-07-26 00:11:16 +0200

    Upgrade infra plugins

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     `kotlin-dsl`
-    id("com.gradle.plugin-publish") version "1.1.0"
-    id("org.hildan.github.changelog") version "1.13.0"
+    id("com.gradle.plugin-publish") version "1.2.0"
+    id("org.hildan.github.changelog") version "2.0.0"
     id("ru.vyarus.github-info") version "1.5.0"
 }
 
@@ -24,14 +24,15 @@ dependencies {
 
 gradlePlugin {
     plugins {
+        @Suppress("UnstableApiUsage")
         create("kotlinPublishPlugin") {
             id = "org.hildan.kotlin-publish"
             displayName = "Kotlin Publish Plugin"
             description = "Configures sources and Javadoc (KDoc) jars for Kotlin JVM, JS and MPP projects"
             implementationClass = "org.hildan.kotlin.publish.KotlinPublishPlugin"
-            website.set("https://github.com/joffrey-bion/gradle-kotlin-publish-plugin")
-            vcsUrl.set("https://github.com/joffrey-bion/gradle-kotlin-publish-plugin")
-            tags.set(listOf("kotlin", "publish", "javadoc", "dokka", "kdoc", "github", "maven"))
+            website = "https://github.com/joffrey-bion/gradle-kotlin-publish-plugin"
+            vcsUrl = "https://github.com/joffrey-bion/gradle-kotlin-publish-plugin"
+            tags = listOf("kotlin", "publish", "javadoc", "dokka", "kdoc", "github", "maven")
         }
     }
 }
