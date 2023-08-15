eacf3976469a3845dc8ebe1252b5f1792f129d22
Joffrey Bion <joffrey.bion@gmail.com>
2023-08-15 18:20:14 +0200

    Enable build scans

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,13 @@
+plugins {
+    id("com.gradle.enterprise") version "3.14.1"
+}
+
 rootProject.name = "gradle-kotlin-publish-plugin"
+
+gradleEnterprise {
+    buildScan {
+        termsOfServiceUrl = "https://gradle.com/terms-of-service"
+        termsOfServiceAgree = "yes"
+        publishAlways()
+    }
+}
